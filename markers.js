9ce1dc229e7dd0f638b7309026d3616053afeff3
Thomas Bowen <133tjb@gmail.com>
2017-11-21 18:33:57 -0800

    fix: Avoid race condition issue with script tag reorder

---
+++ markers.js
@@ -0,0 +1,66 @@
+//Array of marker data
+
+let markers = [];
+
+let nationalHistory = {
+    index: 0,
+    position: {
+        lat: 34.0170,
+        lng: -118.2888,
+    },
+    title: "The National History Museum",
+    description: "The largest natural and historical museum in the western United States",
+    flickrTag: "la+brea+tarpits"
+};
+
+let contemporaryArts = {
+    index: 1,
+    position: {
+        lat: 34.05333,
+        lng: -118.25083,
+    },
+    title: "The Museum of Contemporary Arts",
+    description: "Dedicated to collecting and exhibiting contemporary art",
+    flickrTag: "Museum+of+Contemporary+Arts"
+};
+
+let hammer = {
+    index: 2,
+    position: {
+        lat: 34.0596,
+        lng: -118.4438,
+    },
+    title: "The Hammer Museum",
+    description: "Gallery with a permanent collection of historical works & special exhibits of edgy contemporary art.\n",
+    flickrTag: "hammer+museum"
+};
+
+let getty = {
+    index: 3,
+    position: {
+        lat: 34.0780,
+        lng: -118.4741,
+    },
+    title: "The Getty Museum",
+    description: "A private collection of Western art from the Middle Ages to the present",
+    flickrTag: "the+getty+center"
+};
+
+let iceCream = {
+    index: 4,
+    position: {
+        lat: 34.0342,
+        lng: -118.2316,
+    },
+    title: "The Museum of Ice Cream",
+    description: "Collection of art with ice cream theme",
+    flickrTag: "museum+of+ice+cream"
+};
+
+let locationsObject = {
+    nationalHistory,
+    contemporaryArts,
+    hammer,
+    getty,
+    iceCream
+};
